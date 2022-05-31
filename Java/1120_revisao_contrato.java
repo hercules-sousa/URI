a130b3f44d6fb1e2b994f59c28d0adbfa843d6e5
Hercules de Sousa Silva <hercules.sousa@academico.ifpb.edu.br>
2022-05-31 17:08:31 -0300

    Solving first question in Java

--- Java/1120_revisao_contrato.java
+++ Java/1120_revisao_contrato.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 class Main {
 	public static void main(String[] args) {
-		System.out.println("Aôba");
 		Scanner scanner = new Scanner(System.in);
 
 		String line = scanner.nextLine();
@@ -11,15 +10,29 @@ class Main {
 			String[] splittedLine = line.split(" ");
 			String missingLetter = splittedLine[0];
 			String numberToCheck = splittedLine[1];
-			String answer = new String();
+			String answer = new String("");
 
 			Boolean skipZero = true;
 
 			for (int i = 0; i < numberToCheck.length(); i++) {
 				if (skipZero) {
-					if ()
+					if (numberToCheck.charAt(i) != '0' && !missingLetter.equals(numberToCheck.substring(i, i + 1))) {
+						answer += numberToCheck.charAt(i);
+						skipZero = false;
+					}
+				}
+				else {
+					if (!missingLetter.equals(numberToCheck.substring(i, i + 1))) {
+						answer += numberToCheck.charAt(i);
+					}
 				}
 			}
+
+			if (answer.equals("")) {
+				answer = "0";
+			}
+
+			System.out.println(answer);
 
 			line = scanner.nextLine();
 		}
