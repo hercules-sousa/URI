cc40535a35d3a0e080f73bf64292924ad86b9a6f
Hercules de Sousa Silva <hercules.sousa@academico.ifpb.edu.br>
2022-06-02 09:19:43 -0300

    Solving Bhaskara question in Java

--- Java/1036_formula_bhaskara.java
+++ Java/1036_formula_bhaskara.java
@@ -5,37 +5,51 @@ import java.lang.Math;
 class Main {
  
     public static void main(String[] args) throws IOException {
+		final double THRESHOLD = .00001;
  
 		Scanner scanner = new Scanner(System.in);
 
-		String[] numbers = scanner.nextLine().split(" ");
+		String[] splittedLine = scanner.nextLine().split(" ");
+		String[] numbers = new String[3];
+		int pos = 0;
+		for (int i = 0; i < splittedLine.length; i++) {
+			if (!splittedLine[i].isEmpty()) {
+				numbers[pos] = splittedLine[i];
+				pos++;
+			}
+		}
 
-		float a = Float.parseFloat(numbers[0]);
-		float b = Float.parseFloat(numbers[1]);
-		float c = Float.parseFloat(numbers[2]);
+		double a = Double.parseDouble(numbers[0]);
+		double b = Double.parseDouble(numbers[1]);
+		double c = Double.parseDouble(numbers[2]);
 
-		float delta = b * b - 4 * a * c;
+		if (Math.abs(a) < THRESHOLD) {
+			System.out.println("Impossivel calcular");
+			return;
+		}
 
-		float x1;
-		float x2;
+		double delta = b * b - 4 * a * c;
+
+		double x1;
+		double x2;
 
 		if (delta > 0) {
-			System.out.println("Maior que 0");
-			x1 = (-b + Math.sqrt(delta)) / 2 * a;
-			x2 = (-b - Math.sqrt(delta)) / 2 * a;
+			x1 = (-b + Math.sqrt(delta)) / (2 * a);
+			x2 = (-b - Math.sqrt(delta)) / (2 * a);
 		}
 		else {
 			if (delta == 0) {
-				x1 = -b / 2 * a;
+				x1 = -b / (2 * a);
 				x2 = x1;
 			}
 			else {
+				System.out.println("Impossivel calcular");
 				return;
 			}
 		}
 
-		System.out.println(x1);
-		System.out.println(x2);
+		System.out.printf("R1 = %.5f\n", x1);
+		System.out.printf("R2 = %.5f\n", x2);
 
     }
  
